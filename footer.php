2e092c638a482190deb33052788055b52ae51da1
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-06-08 13:59:42 -0700

    code destructuring and adjustment

--- footer.php
+++ footer.php
@@ -11,67 +11,7 @@
 </div>
 </div>
 
-<!-- jquery
-		============================================ -->
-<script src="js/vendor/jquery-1.12.4.min.js"></script>
-<!-- bootstrap JS
-		============================================ -->
-<script src="js/bootstrap.min.js"></script>
-<!-- wow JS
-		============================================ -->
-<script src="js/wow.min.js"></script>
-<!-- price-slider JS
-		============================================ -->
-<script src="js/jquery-price-slider.js"></script>
-<!-- meanmenu JS
-		============================================ -->
-<script src="js/jquery.meanmenu.js"></script>
-<!-- owl.carousel JS
-		============================================ -->
-<script src="js/owl.carousel.min.js"></script>
-<!-- sticky JS
-		============================================ -->
-<script src="js/jquery.sticky.js"></script>
-<!-- scrollUp JS
-		============================================ -->
-<script src="js/jquery.scrollUp.min.js"></script>
-<!-- counterup JS
-		============================================ -->
-<script src="js/counterup/jquery.counterup.min.js"></script>
-<script src="js/counterup/waypoints.min.js"></script>
-<script src="js/counterup/counterup-active.js"></script>
-<!-- mCustomScrollbar JS
-		============================================ -->
-<script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="js/scrollbar/mCustomScrollbar-active.js"></script>
-<!-- metisMenu JS
-		============================================ -->
-<script src="js/metisMenu/metisMenu.min.js"></script>
-<script src="js/metisMenu/metisMenu-active.js"></script>
-<!-- morrisjs JS
-		============================================ -->
-<script src="js/morrisjs/raphael-min.js"></script>
-<script src="js/morrisjs/morris.js"></script>
-<script src="js/morrisjs/morris-active.js"></script>
-<!-- morrisjs JS
-		============================================ -->
-<script src="js/sparkline/jquery.sparkline.min.js"></script>
-<script src="js/sparkline/jquery.charts-sparkline.js"></script>
-<script src="js/sparkline/sparkline-active.js"></script>
-<!-- calendar JS
-		============================================ -->
-<script src="js/calendar/moment.min.js"></script>
-<script src="js/calendar/fullcalendar.min.js"></script>
-<script src="js/calendar/fullcalendar-active.js"></script>
-<!-- plugins JS
-		============================================ -->
-<script src="js/plugins.js"></script>
-<!-- main JS
-		============================================ -->
-<script src="js/main.js"></script>
-<!-- tawk chat JS
-		============================================ -->
-<script src="js/tawk-chat.js"></script>
+
 </body>
 
 </html>
